b63467f7493a3c74c1ad45f31d3669b30e760c3d
Evans Nyarko <evans.nyarko39@yahoo.com>
2024-01-03 16:49:32 +0000

    errors

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return (left > right ? left : right);
+	return ((left > right) ? left : right);
 }
 
 /**
@@ -28,12 +28,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t bf;
-
 	if (tree == NULL)
 		return (0);
 
-	bf = binary_tree_height(tree->left) - binary_tree_height(tree->right);
-
-	return ((int)bf);
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
